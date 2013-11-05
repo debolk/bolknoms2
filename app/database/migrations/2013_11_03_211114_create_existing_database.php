a8749e3849d6bbd38b12a6cefbbf7b76e20703fa
Jakob Buis <jpabuis@gmail.com>
2013-11-05 14:38:38 +0100

    migrated migrations

---
+++ app/database/migrations/2013_11_03_211114_create_existing_database.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExistingDatabase extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('meals', function($table){
+              $table->increments('id');
+              $table->date('date');
+              $table->time('locked');
+              $table->string('event');
+              $table->boolean('promoted');
+              $table->timestamps();
+            });
+            Schema::create('registrations', function($table){
+              $table->increments('id');
+              $table->string('name');
+              $table->integer('meal_id')->unsigned();
+              $table->foreign('meal_id')->references('id')->on('meals');
+              $table->string('email');
+              $table->string('salt');
+              $table->timestamps();
+            });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('registrations');
+        Schema::drop('meals');
+	}
+
+}
