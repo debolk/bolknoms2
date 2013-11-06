66ea4fcf39d8092e73ab36a260a2fbf025e501ac
Jakob Buis <jpabuis@gmail.com>
2013-11-06 17:14:59 +0100

    refactored queries for statistics

--- app/models/Registration.php
+++ app/models/Registration.php
@@ -4,13 +4,7 @@ class Registration extends Eloquent
 {
   public static function top_ytd($count = 10)
   {
-    $query = DB::table('registrations');
-    $query->select(DB::raw('name, COUNT(name) as count'));
-    $query->leftJoin('meals', 'registrations.meal_id', '=', 'meals.id');
-    $query->where('meals.date', '<=', 'NOW()');
-    $query->groupBy('name');
-    $query->orderBy('count');
-    $query->take($count);
+    $query = self::statistics($count);
 
     // Determine last 1-sep
     if (time() > strtotime('01 September')) {
@@ -25,6 +19,18 @@ class Registration extends Eloquent
 
   public static function top_alltime($count = 10)
   {
-    return DB::table('registrations')->select(DB::raw('name, COUNT(name) AS count'))->leftJoin('meals', 'registrations.meal_id', '=', 'meals.id')->where('meals.date', '<=', 'NOW()')->groupBy('name')->orderBy('count')->take($count)->get();
+    return self::statistics($count)->get();
+  }
+
+  private static function statistics($count)
+  {
+    $query = DB::table('registrations');
+    $query->select(DB::raw('name, COUNT(name) as count'));
+    $query->leftJoin('meals', 'registrations.meal_id', '=', 'meals.id');
+    $query->where('meals.date', '<=', 'NOW()');
+    $query->groupBy('name');
+    $query->orderBy('count');
+    $query->take($count);
+    return $query;
   }
 }
