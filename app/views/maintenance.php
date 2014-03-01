999fcac1b15ab5516edc08ea3a32d6e8f44622cb
Jakob Buis <jpabuis@gmail.com>
2014-03-01 18:35:34 +0100

    implemented somewhat basic maintenance page

---
+++ app/views/maintenance.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bolknoms in onderhoud</title>
+</head>
+<body>
+    <h1>Onderhoud bezig</h1>
+    <p>
+        Op dit moment is er onderhoud bezig aan bolknoms. Excuses voor het ongemak. Meestal duurt dit minder dan 15 minuten.
+        In de tussentijd kun je je vermaken met de <a href="http://www.youtube.com/results?search_query=swedish%20chef">Swedish Chef</a>. 
+    </p>
+</body>
+</html>
