1ee949906797f8ca945479710dded4522e08ea1a
Jakob Buis <jpabuis@gmail.com>
2014-09-04 13:14:05 +0200

    another configuration bugfix

--- app/config/mail.php
+++ app/config/mail.php
@@ -4,7 +4,7 @@ return array(
   'driver' => 'smtp',
   'host' => 'smtp.sendgrid.net',
   'port' => 587,
-  'from' => array('address' => getenv('EMAIL_REPLYTO_FROM'), 'name' => getenv('EMAIL_REPLYTO_NAME')),
+  'from' => array('address' => getenv('EMAIL_REPLYTO_EMAIL'), 'name' => getenv('EMAIL_REPLYTO_NAME')),
   'encryption' => 'tls',
   'username' => getenv('SENDGRID_USERNAME'),
   'password' => getenv('SENDGRID_PASSWORD'),
