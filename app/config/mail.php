222ca866d5040aedb0658a399e6ac39e152e6014
Jakob Buis <jpabuis@gmail.com>
2014-09-04 13:11:54 +0200

    bugfix in configuration variables

--- app/config/mail.php
+++ app/config/mail.php
@@ -4,7 +4,7 @@ return array(
   'driver' => 'smtp',
   'host' => 'smtp.sendgrid.net',
   'port' => 587,
-  'from' => array('address' => getenv('EMAIL_FROM'), 'name' => getenv('EMAIL_NAME')),
+  'from' => array('address' => getenv('EMAIL_REPLYTO_FROM'), 'name' => getenv('EMAIL_REPLYTO_NAME')),
   'encryption' => 'tls',
   'username' => getenv('SENDGRID_USERNAME'),
   'password' => getenv('SENDGRID_PASSWORD'),
