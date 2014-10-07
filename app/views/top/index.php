9516cf965e87c0f0dc474bca03ffd60f68ef8dda
Jakob Buis <jpabuis@gmail.com>
2014-10-07 15:39:02 +0200

    bugfix in statistics: wrong data set is used

--- app/views/top/index.php
+++ app/views/top/index.php
@@ -7,7 +7,7 @@
 
 <h1>Top eters dit all-time</h1>
 <ol>
-    <?php foreach ($statistics_ytd as $registration): ?>
+    <?php foreach ($statistics_alltime as $registration): ?>
         <li><?php echo $registration->name; ?> (<?php echo $registration->count;?>)</li>
     <?php endforeach; ?>
 </ol>
