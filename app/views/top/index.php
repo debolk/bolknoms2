1fec7797de0ae27e958bb0aeb6a392f743c46af0
Jakob Buis <jpabuis@gmail.com>
2014-10-07 16:20:36 +0200

    added counts

--- app/views/top/index.php
+++ app/views/top/index.php
@@ -1,11 +1,11 @@
-<h1>Top eters dit jaar</h1>
+<h1>Top eters dit jaar (<?= sizeof($statistics_ytd); ?> eters)</h1>
 <ol>
     <?php foreach ($statistics_ytd as $registration): ?>
         <li><?php echo $registration->name; ?> (<?php echo $registration->count;?>)</li>
     <?php endforeach; ?>
 </ol>
 
-<h1>Top eters all-time</h1>
+<h1>Top eters all-time (<?= sizeof($statistics_alltime); ?> eters)</h1>
 <ol>
     <?php foreach ($statistics_alltime as $registration): ?>
         <li><?php echo $registration->name; ?> (<?php echo $registration->count;?>)</li>
