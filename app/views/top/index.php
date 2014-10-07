028bbd2e80cac8d2bd97dc3c1d34059c800256c2
Jakob Buis <jpabuis@gmail.com>
2014-10-07 15:59:32 +0200

    fix spelling mistake

--- app/views/top/index.php
+++ app/views/top/index.php
@@ -5,7 +5,7 @@
     <?php endforeach; ?>
 </ol>
 
-<h1>Top eters dit all-time</h1>
+<h1>Top eters all-time</h1>
 <ol>
     <?php foreach ($statistics_alltime as $registration): ?>
         <li><?php echo $registration->name; ?> (<?php echo $registration->count;?>)</li>
