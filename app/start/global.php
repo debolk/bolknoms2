a6df279f73937f4efca62f9156ebd905f3c4011b
Jakob Buis <jpabuis@gmail.com>
2014-11-10 01:04:55 +0100

    now using phpdotenv for configuration

--- app/start/global.php
+++ app/start/global.php
@@ -49,29 +49,32 @@ Log::useFiles(storage_path().'/logs/laravel.log');
 // Generic error handler logs the stack trace
 App::error(function(Exception $exception, $code)
 {
-    // Do not handle errors in debug mode
-    if (Config::get('app.debug')) {
-        return;
-    }
+    // Do not handle any error
+    return;
+    
+    // // Do not handle errors in debug mode
+    // if (Config::get('app.debug')) {
+    //     return;
+    // }
 
-    // Log relevant data
-    $log = "$code at " . Request::url();
-    if ($code >= 500) {
-        $log .= ". Stack trace:\n$exception";
-    }
-    Log::error($log);
+    // // Log relevant data
+    // $log = "$code at " . Request::url();
+    // if ($code >= 500) {
+    //     $log .= ". Stack trace:\n$exception";
+    // }
+    // Log::error($log);
 
-    // Send notification to the technical administrator, except for common errors we don't want to see
-    if (in_array($code, [403, 404])) {
-        $reported_automatically = false;
-    }
-    else {
-        MailerBug::send_bug_notification($log);
-        $reported_automatically = true;
-    }
+    // // Send notification to the technical administrator, except for common errors we don't want to see
+    // if (in_array($code, [403, 404])) {
+    //     $reported_automatically = false;
+    // }
+    // else {
+    //     MailerBug::send_bug_notification($log);
+    //     $reported_automatically = true;
+    // }
 
-    // Show a friendly error page
-    return Response::view('error/index', ['code' => $code, 'reported_automatically' => $reported_automatically], $code);
+    // // Show a friendly error page
+    // return Response::view('error/index', ['code' => $code, 'reported_automatically' => $reported_automatically], $code);
 });
 
 /*
