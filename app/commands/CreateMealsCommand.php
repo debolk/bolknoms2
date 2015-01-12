046696c4f0c7438989b829c850da30a9619d7851
Jakob Buis <jpabuis@gmail.com>
2015-01-12 18:08:29 +0100

    convert indentation to spaces

--- app/commands/CreateMealsCommand.php
+++ app/commands/CreateMealsCommand.php
@@ -5,36 +5,36 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 
 /**
- * Automatically create meals in the database for this 
+ * Automatically create meals in the database for this
  */
 class CreateMealsCommand extends Command {
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'meals:generate';
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'meals:generate';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Generate the meals for this week and the next';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate the meals for this week and the next';
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function fire()
-	{
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
         // Get next monday
         $date = strtotime('next monday');
 
         // Walk until the thursday (4 days)
-        for ($i=0; $i < 4; $i++) { 
+        for ($i=0; $i < 4; $i++) {
             $current_date = date('Y-m-d', strtotime("+{$i} days", $date));
             echo "Attempting {$current_date}...";
             if (Meal::withTrashed()->where('date', '=', $current_date)->count() == 0) {
@@ -45,5 +45,5 @@ class CreateMealsCommand extends Command {
                 echo "exists\n";
             }
         }
-	}
+    }
 }
