81dc43ae1b6233db0bf1a7f00c92303d01052361
Jakob Buis <jpabuis@gmail.com>
2015-02-26 23:27:00 +0100

    extracted javascript to separate files per controller

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -8,6 +8,14 @@ class ApplicationController extends Controller
      */
     public function __construct()
     {
-      $this->layout = View::make('application/application');
+        $this->layout = View::make('application/application');
+
+        // Find the desired Javascript file
+        if (preg_match('/^([a-zA-Z]+)Controller/', Route::currentRouteAction(), $controller)) {
+            $file = strtolower($controller[1]);
+            if (file_exists(dirname(__FILE__) . "/../../public/javascripts/$file.js")) {
+                $this->layout->javascript = $file;
+            }
+        }
     }
 }
