e58cf7fac71f3ca36ec6e61a8e65972258ac4b2b
Jakob Buis <jpabuis@gmail.com>
2015-03-15 14:46:23 +0100

    removed reference to old jquery library

--- app/views/error/index.php
+++ app/views/error/index.php
@@ -4,16 +4,9 @@
     <meta charset="utf-8"/>
     <title>bolknoms</title>
 
-    <link rel="stylesheet" href="/stylesheets/jquery-ui.css" type="text/css"/>
     <link rel="stylesheet" href="/stylesheets/application.css" type="text/css"/>
     <link href='http://fonts.googleapis.com/css?family=Cardo:400,400italic,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="stylesheets/app.css">
-
-    <script type="text/javascript" src="/javascripts/jquery.js"></script>
-    <script type="text/javascript" src="/javascripts/jquery-ui.js"></script>
-    <script type="text/javascript" src="/javascripts/date-format.js"></script>
-    <script type="text/javascript" src="/javascripts/application.js"></script>
-    <script type="text/javascript" src="javascripts/app.js"></script>
 
     <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" />
 </head>
