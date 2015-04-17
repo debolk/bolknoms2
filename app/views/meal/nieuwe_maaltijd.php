e3a2ac71f43f8dfa84f46da49b350f982d7c88f7
Jakob Buis <jpabuis@gmail.com>
2015-04-17 21:20:03 +0200

    use text fields for date and time input, browser support is currently insufficient

--- app/views/meal/nieuwe_maaltijd.php
+++ app/views/meal/nieuwe_maaltijd.php
@@ -5,11 +5,11 @@
 <form action="/administratie/nieuwe_maaltijd_maken" method="post" accept-charset="utf-8" class="clearfix">
     <p>
         <label class="label" for="date">Datum</label><br>
-        <input type="date" placeholder="<?= date('d-m-Y'); ?>" autofocus name="date" min="<?=date('Y-m-d');?>">
+        <input type="text" placeholder="<?= date('d-m-Y'); ?>" autofocus name="date">
     </p>
     <p>
         <label class="label" for="locked">Inschrijving sluit</label><br>
-        <input type="time" placeholder="15:00" name="locked">
+        <input type="text" placeholder="15:00" name="locked">
     </p>
     <p>
         <input type="submit" value="Maaltijd toevoegen" />
