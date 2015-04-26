94ebc34e9df46124ffe825b52091e786ccb13f2a
Jakob Buis <jpabuis@gmail.com>
2015-04-26 14:58:49 +0200

    improved instructions upon refusing oauth

--- resources/views/oauth/denied.php
+++ resources/views/oauth/denied.php
@@ -1,4 +1,7 @@
 <h3>Toegang geweigerd</h3>
 <p>
-    Je hebt geweigerd om de gebruikersnaam van je Bolkaccount met Bolknoms te delen. Dat is onhandig, want nu weten we niet wie je bent en mag je niet naar binnen. Bolknoms krijgt verder geen toegang tot jouw Bolkaccount. Als je wilt, kun je het <a href="/<?=$url;?>">opnieuw proberen</a>.
+    Je hebt geweigerd om de gebruikersnaam van je Bolkaccount met Bolknoms te delen. Dat is onhandig, want nu weten we niet wie je bent en mag je niet naar binnen. Als je wilt, kun je het <a href="/<?=$url;?>">opnieuw proberen</a>.
+</p>
+<p>
+    Bolknoms gebruikt alleen je gebruikersnaam om te bepalen wie je bent en of je bestuur bent. Met de rest van je Bolk-account doen we verder niets.
 </p>
