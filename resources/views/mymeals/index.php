36a2444d139f437ff507526d3d395c963dcc9e69
Jakob Buis <jpabuis@gmail.com>
2015-04-27 19:48:10 +0200

    add a zero-case message

--- resources/views/mymeals/index.php
+++ resources/views/mymeals/index.php
@@ -1,10 +1,11 @@
 <h1>Mijn maaltijden</h1>
-<p>
-    Een lijst met al je maaltijden.
-</p>
 
-<div class="meals">
-    <?php foreach ($registrations as $registration): ?>
-        <?= $registration->meal; ?> <br>
-    <?php endforeach; ?>
-</div>
+<?php if (count($registrations) > 0): ?>
+    <div class="meals">
+        <?php foreach ($registrations as $registration): ?>
+            <?= $registration->meal; ?> <br>
+        <?php endforeach; ?>
+    </div>
+<?php else: ?>
+    <p class="zero">Je hebt je nog nooit aangemeld voor een maaltijd.</p>
+<?php endif; ?>
