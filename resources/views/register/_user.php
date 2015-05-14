b4bd3978247fa4b555c552ea28e30a9f67b51c57
Jakob Buis <jpabuis@gmail.com>
2015-05-14 23:57:53 +0200

    UI fixes

--- resources/views/register/_user.php
+++ resources/views/register/_user.php
@@ -1,9 +1,11 @@
 <div class="user">
-    <a class="button" href="/logout">Uitloggen</a>
     <img src="/photo" alt="Foto van Jakob Buis" class="photo">
-    <h3 class="name"><?= $user->name; ?></h3>
-    <div class="details">
-        <a href="#" class="handicap">Geen dieetwensen</a> <br>
-        <span class="count">37</span>x meegegeten (<a href="/top-eters">#1</a>)
-    </div>
+        <h3 class="name"><?= $user->name; ?></h3>
+
+        <a class="button" href="/logout">Uitloggen</a>
+        <div class="details">
+            Geen dieetwensen |
+            <span class="count">37</span>x meegegeten (<a href="/top-eters">#1</a>) |
+            <a href="/profiel">Mijn profiel</a>
+        </div>
 </div>
