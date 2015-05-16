36d7ef27fc5febc8e0afe55faaf87326912cabf0
Jakob Buis <jpabuis@gmail.com>
2015-05-16 23:48:31 +0200

    removed rules that are not implemented yet

--- resources/views/page/spelregels.php
+++ resources/views/page/spelregels.php
@@ -7,11 +7,9 @@
 <ol>
     <li>Je kunt niet mee-eten zonder je aan te melden.</li>
     <li>Je kunt je aanmelden tot de vastgestelde sluitingstijd (meestal 15:00 uur). De sluitingstijd wordt bepaald door het bestuur en staat vermeld bij de maaltijd.</li>
-    <li>Externen kunnen zich aanmelden voor de eerstvolgende maaltijd. Na aanmelden krijg je een e-mail met een bevestigingslink. Als je je aanmelding niet bevestigt, wordt je aanmelding verwijderd. Bolkers kunnen zich aanmelden voor alle open maaltijden. Ze hoeven ook niet te bevestigen via e-mail.</li>
-    <li>Bolkers kunnen ook extra personen (+1, +2, &hellip;) inschrijven voor de maaltijd. Je moet dan wel zelf ook mee-eten.</li>
+    <li>Externen kunnen zich aanmelden voor de eerstvolgende maaltijd. Bolkers kunnen zich aanmelden voor alle open maaltijden.</li>
     <li>Je kunt je weer afmelden tot aan de sluitingstijd. Als je met je Bolkaccount aangemeld bent, doe je
     dat via de website. Als je geen Bolkaccount hebt, meld je je af door het bestuur te bellen op <a href="tel:+31152126012">015 212 60 12</a>.</li>
-    <li>Het kan zijn dat de maaltijd verandert (sluitingstijd, etenstijd, etc.) als je al bent aangemeld. Als dat gebeurt, sturen we je een e-mail.</li>
 </ol>
 
 <h2>Mee-eten</h2>
@@ -26,12 +24,4 @@
     <li>Als je niet op tijd afmeldt en niet komt opdagen, moet je wel voor de maaltijd betalen (meestal &euro;3,50). Er is immers wel voor je gekookt. Voor leden gaat dat op Bolkrekening, externen krijgen een factuur gemaild.</li>
     <li>Bolkers zijn verantwoordelijk voor de betaling van de +1's die ze aanmelden.</li>
     <li>Als je de kosten van de gemiste maaltijd niet betaalt, kun je niet meer mee-eten aan de eettafel.</li>
-</ol>
-
-<h2>Abonnementen</h2>
-<ol>
-    <li>Je kunt een abonnement instellen om automatisch mee te eten op bepaalde dagen van de week. Hiervoor moet je inloggen met je Bolkaccount.</li>
-    <li>Je kunt een e-mailreminder instellen voor je abonnement. Je krijgt dan elke maandag een e-mail met een lijst van maaltijden waarvoor je bent aangemeld.</li>
-    <li>Je bent zelf verantwoordelijk om je af te melden als je een keer niet komt. Gemiste maaltijden moet je betalen zoals elke gemiste maaltijd.</li>
-    <li>Als je twee maaltijden direct na elkaar niet komt opdagen, wordt je abonnement automatisch stopgezet. Je ontvangt hierover een e-mail.</li>
 </ol>
