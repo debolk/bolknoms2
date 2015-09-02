031e6533641de2b090da361de6329324398b56b3
Jakob Buis <jakob@cubiso.nl>
2015-09-02 02:34:30 +0200

    use ApiController as parent, implemented parameters from and to

--- app/Http/Controllers/API/MealsController.php
+++ app/Http/Controllers/API/MealsController.php
@@ -1,20 +1,47 @@
 <?php namespace App\Http\Controllers\API;
 
 use App\Http\Requests;
-use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 use App\Models\Meal;
 
-class MealsController extends Controller {
+class MealsController extends ApiController
+{
 
 	/**
 	 * Display a listing of the resource.
 	 *
 	 * @return Response
 	 */
-	public function index()
+	public function index(Request $request)
 	{
-		return response()->json(Meal::all());
+        // Validate input parameters
+        $validator = \Validator::make($request->all(), [
+            'from' => ['regex:/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/', 'date'],
+            'to' => ['regex:/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/', 'date']
+        ],[
+            'from.regex' => 'from parameter must be in the format YYYY-MM-DD',
+            'from.date' => 'from parameter must be in a valid date',
+            'to.regex' => 'to parameter must be in the format YYYY-MM-DD',
+            'to.date' => 'to parameter must be in a valid date'
+        ]);
+        if (!$validator->passes()) {
+            return $this->fatalError(400, 'parameters_unacceptable', $validator->messages());
+        }
+
+        $meals = Meal::query();
+
+        // Limit by dates if requested
+        $from = $request->input('from', null);
+        if ($from) {
+            $meals = $meals->where('date', '>=', $from);
+        }
+        $to = $request->input('to', null);
+        if ($to) {
+            $meals = $meals->where('date', '<=', $to);
+        }
+
+        // Return JSON-encoded meals
+		return response()->json($meals->get());
 	}
 
 	/**
