58e85d0b65bc34e3a06bf1f81257ddaf393554a9
Jakob Buis <jakob@cubiso.nl>
2015-09-02 12:06:07 +0200

    updated to laravel 5.1

--- config/app.php
+++ config/app.php
@@ -140,6 +140,7 @@ return array(
         'Illuminate\Translation\TranslationServiceProvider',
         'Illuminate\Validation\ValidationServiceProvider',
         'Illuminate\View\ViewServiceProvider',
+        'Illuminate\Broadcasting\BroadcastServiceProvider',
         /*
          * Application Service Providers...
          */
