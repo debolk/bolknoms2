5fe9d5f451b254f217a67077406ca46c6eda90bd
Jakob Buis <jakob@cubiso.nl>
2015-09-08 19:09:57 +0200

    Added ConfirmRegistrationService

---
+++ app/Services/ConfirmRegistrationService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Services;
+
+use Log;
+use Validator;
+use DateTime;
+use App\Models\Registration;
+use Exception;
+
+class ConfirmRegistrationService extends Service
+{
+    /**
+     * Set the Service
+     * @param array $data data for the new Meal
+     */
+    public function __construct(Registration $registration, $salt)
+    {
+        $this->registration = $registration;
+        $this->salt = $salt;
+    }
+
+    /**
+     * Confirm a registration
+     * @return App\Models\Meal the newly created meal
+     * @throws SaltMismatchException
+     * @throws MealDeadlinePassedException
+     * @return Registration
+     */
+    public function execute()
+    {
+        // Salt must match
+        if ($this->registration->salt !== $this->salt) {
+            throw new SaltMismatchException;
+        }
+
+        // Deadline must not have passed
+        if (! $this->registration->meal->open_for_registrations()) {
+            throw new MealDeadlinePassedException;
+        }
+
+        // Confirm registration
+        $this->registration->confirmed = true;
+        if (!$this->registration->save()) {
+            return null;
+        }
+
+        Log::notice("Registration {$this->registration->id} bevestigd");
+        return $this->registration;
+    }
+}
+
+class SaltMismatchException extends Exception {}
+class MealDeadlinePassedException extends Exception {}
