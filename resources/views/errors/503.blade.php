e23a73b7862e38112dca4030465a8baa4b571a54
Jakob Buis <jakob@cubiso.nl>
2015-11-12 15:48:28 +0100

    another typo

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -11,7 +11,7 @@
 <body>
     <h1>Bolknoms offline</h1>
     <p>
-        Bolknoms is tijdelijk offline wegens onderhoud of een andere reden. Meestal duurt dit korter dan 10 minuten. Mocht het niet lukken om je aan te melden, of bolknoms blijft offline, dan vind je de meest recente informatie in <a href="https://www.facebook.com/groups/176904635721261/">de Facebook-groep van de De Bolk</a>. Meestal kun je je daar ook direct aanmelden voor de maaltijd. Of bel het bestuur op <a href="telL+31152126012">015 212 60 12</a>.
+        Bolknoms is tijdelijk offline wegens onderhoud of een andere reden. Meestal duurt dit korter dan 10 minuten. Mocht het niet lukken om je aan te melden, of bolknoms blijft offline, dan vind je de meest recente informatie in <a href="https://www.facebook.com/groups/176904635721261/">de Facebook-groep van de De Bolk</a>. Meestal kun je je daar ook direct aanmelden voor de maaltijd. Of bel het bestuur op <a href="tel:+31152126012">015 212 60 12</a>.
     </p>
 </body>
 </html>
