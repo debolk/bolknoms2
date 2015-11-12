18ab835e6a35c6dbbd3d3c6ad362044d035ab698
Jakob Buis <jakob@cubiso.nl>
2015-11-12 15:40:30 +0100

    clear maintenance page with instructions

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <title>Bolknoms offline</title>
+
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=yes" />
+
+    <link rel=icon href="/images/favicon.png" sizes="196x196" type="image/png">
+</head>
+<body>
+    <h1>Bolknoms offline</h1>
+    <p>
+        Bolknoms is tijdelijk offline wegens onderhoud of een andere reden. Meestal duurt dit korter dan 10 minuten. Mocht het niet lukken om je aan te melden, of bolknoms blijft online, dan vind je de meest recente informatie in <a href="https://www.facebook.com/groups/176904635721261/">de Facebook-groep van de De Bolk</a>. Meestal kun je je daar ook direct aanmelden voor de maaltijd. Of bel het bestuur op <a href="telL+31152126012">015 212 60 12</a>.
+    </p>
+</body>
+</html>
