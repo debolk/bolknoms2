c640f3d9dcf383dc9badecee935675a794d83247
Jakob Buis <jakob@jakobbuis.nl>
2015-11-28 14:32:40 +0100

    meal deadlines must be before the starting of the meal

--- app/Services/CreateMealService.php
+++ app/Services/CreateMealService.php
@@ -28,7 +28,7 @@ class CreateMealService extends Service
         // Validate the resulting input
         $validator = Validator::make($this->data, [
             'meal_timestamp'   => ['date_format:d-m-Y G:i', 'required', 'after:now', 'unique:meals'],
-            'locked_timestamp' => ['date_format:d-m-Y G:i', 'required', 'after:now']
+            'locked_timestamp' => ['date_format:d-m-Y G:i', 'required', 'after:now', 'before:meal_timestamp']
         ],[
              'meal_timestamp.date_format'   => 'De ingevulde maaltijd is ongeldig (formaat DD-MM-YYYY HH:MM)',
              'meal_timestamp.required'      => 'De ingevulde maaltijd is ongeldig (formaat DD-MM-YYYY HH:MM)',
@@ -36,7 +36,8 @@ class CreateMealService extends Service
              'meal_timestamp.unique'        => 'Er is al een maaltijd op deze datum en tijd',
              'locked_timestamp.date_format' => 'De ingevulde sluitingstijd is ongeldig (formaat DD-MM-YYYY HH:MM)',
              'locked_timestamp.required'    => 'De ingevulde sluitingstijd is ongeldig (formaat DD-MM-YYYY HH:MM)',
-             'locked_timestamp.after'       => 'De deadline voor aanmelding mag niet al geweest zijn'
+             'locked_timestamp.after'       => 'De deadline voor aanmelding mag niet al geweest zijn',
+             'locked_timestamp.before'      => 'De sluitingstijd moet voor het begin van de maaltijd liggen',
         ]);
 
         if ($validator->fails()) {
