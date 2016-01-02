5791b43685ccb7f0b1472dca3147194cdedc6489
Jakob Buis <jakob@jakobbuis.nl>
2016-01-02 21:41:07 +0100

    added first working test ever

---
+++ tests/RegisterTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\Meal;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class RegisterTest extends TestCase
+{
+    public function testCanSeeAnOpenMeal()
+    {
+        $meal = factory(Meal::class)->create([
+            'meal_timestamp' => strtotime('+2 hours'),
+            'locked_timestamp' => strtotime('+1 hour'),
+        ]);
+
+        $request = $this->action('GET', 'Register@index');
+
+        $this->assertResponseOk();
+        $this->see( (string) $meal);
+    }
+}
