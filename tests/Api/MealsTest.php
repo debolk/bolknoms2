3368981b9c518a04a4bde6cbd2f8f3a5d3f178a2
Jakob Buis <jakob@jakobbuis.nl>
2016-01-02 21:52:46 +0100

    added (failing!) API tests

---
+++ tests/Api/MealsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Meal;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class Mealstest extends TestCase
+{
+    public function testRationality()
+    {
+        $this->assertEquals(1 + 1, 2);
+    }
+
+    public function testListOpenMeals()
+    {
+        $meal = factory(Meal::class)->create([
+            'meal_timestamp' => strtotime('in two hours'),
+            'locked_timestamp' => strtotime('in one hour'),
+        ]);
+
+        $request = $this->action('GET', 'Api\MealsController@open');
+
+        $this->assertResponseOk();
+        $request->seeJson(['id' => $meal->id]);
+    }
+}
