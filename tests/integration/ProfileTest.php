0723f062beaaae2f0110afe18463e96af6650126
Jakob Buis <jakob@jakobbuis.nl>
2016-01-23 20:24:50 +0100

    added tests for profiles

--- tests/integration/ProfileTest.php
+++ tests/integration/ProfileTest.php
@@ -1,34 +1,52 @@
 <?php
 
+use App\Http\Helpers\OAuth;
+use App\Models\Registration;
+use App\Models\User;
+use Illuminate\Support\Facades\Session;
+
 class ProfileTest extends TestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create(['handicap' => 'Heel veel vlees']);
+        $this->loginAs($this->user);
+    }
+
     /** @test */
     public function can_view_your_profile()
     {
-        $this->markTestIncomplete();
+        $this->visit('/profiel');
+        $this->see('Mijn profiel');
+        $this->assertResponseOk();
     }
 
     /** @test */
     public function profile_contains_your_handicap()
     {
-        $this->markTestIncomplete();
+        $this->visit('/profiel');
+        $this->see($this->user->handicap);
+        $this->assertResponseOk();
     }
 
     /** @test */
     public function can_set_your_handicap()
     {
-        $this->markTestIncomplete();
+        $this->post('/handicap', ['handicap' => 'geen vlees meer']);
+        $this->assertResponseOk();
+        $this->seeInDatabase('users', ['id' => $this->user->id, 'handicap' => 'geen vlees meer']);
     }
 
     /** @test */
     public function can_view_attended_meals()
     {
-        $this->markTestIncomplete();
-    }
+        $registration = factory(Registration::class, 'userRegistration')->create();
+        $this->loginAs($registration->user);
 
-    /** @test */
-    public function can_view_your_profile_picture()
-    {
-        $this->markTestIncomplete();
+        $this->visit('/profiel');
+        $this->assertResponseOk();
+        $this->see($registration->meal->longDate());
     }
 }
