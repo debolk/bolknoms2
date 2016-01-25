78c0801efdc537bf7ab77e22eb7b2fe01cb527ab
Jakob Buis <jakob@jakobbuis.nl>
2016-01-25 12:03:46 +0100

    deploying should clear all caches first

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 php artisan down
+php artisan clear-compiled
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
 git fetch
 git merge origin/master master
 php composer.phar install
 gulp --production
 php artisan migrate
-php artisan view:clear
 php artisan up
