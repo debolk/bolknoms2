9bed28bab37386b7299c39f01948fe98cac40924
Jakob Buis <jakob@jakobbuis.nl>
2018-01-22 21:11:36 +0100

    move command to its own namespace, extract RegistrationsIterator into a separate file

--- app/Console/Commands/Tensorflow/TensorflowData.php
+++ app/Console/Commands/Tensorflow/TensorflowData.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Console\Commands\Tensorflow;
 
 use Illuminate\Console\Command;
 use App\Models\Meal;
@@ -194,47 +194,3 @@ class TensorflowData extends Command
         fwrite(STDERR, $text . "\n");
     }
 }
-
-// 'Replays' known registrations.
-class RegistrationsIterator
-{
-    private $registrations;
-    private $currentIndex = 0;
-    private $currentTimestamp = 0;
-    private $currentCounts = [];
-
-    function __construct($registrations)
-    {
-        $this->registrations = $registrations;
-    }
-
-    function forwardTo($timestamp)
-    {
-        if ($timestamp < $this->currentTimestamp) {
-            throw new Exception();
-        }
-        $this->currentTimestamp = $timestamp;
-
-        while ($this->currentIndex < count($this->registrations) && $this->registrations[$this->currentIndex][0] <= $timestamp)
-        {
-            $r = $this->registrations[$this->currentIndex];
-            $mealId = $r[1];
-            if (!isset($this->currentCounts[$mealId]))
-                $this->currentCounts[$mealId] = 0;
-
-            $this->currentCounts[$mealId]++;
-
-            $this->currentIndex++;
-        }
-
-        return 0;
-    }
-
-    function get($id)
-    {
-        if(!isset($this->currentCounts[$id]))
-            return 0;
-        return $this->currentCounts[$id];
-    }
-}
-
