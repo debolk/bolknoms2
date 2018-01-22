a87391e2a1f52c3f973a5724cad279dd275e777a
Jakob Buis <jakob@jakobbuis.nl>
2018-01-22 21:36:20 +0100

    code formatting, improved semantics and better comments

--- app/Console/Commands/Tensorflow/TensorflowData.php
+++ app/Console/Commands/Tensorflow/TensorflowData.php
@@ -25,13 +25,14 @@ class TensorflowData extends Command
     {
         ini_set('max_execution_time', 0);
 
-        $headers = [
+        // Print header row in CSV for interpretation
+        $this->printRow([
             'timestamp_meal',
-            'day_of_week',
+            'day_of_week', // int, Monday = 1, Sunday = 7
             'timestamp_sample',
             'minutes_left_until_deadline',
             'count_registrations',
-            'c-1',
+            'c-1', // c is the current meal date, c-1 = yesterday, etc.
             'c-2',
             'c-3',
             'c-4',
@@ -45,18 +46,18 @@ class TensorflowData extends Command
             'p-5',
             'p-6',
             'p-7',
-            'p+0',
-            'p+1',
+            'p+0', // p is the same day, last year as per ISO
+            'p+1', // p+1 last year tomorrow, etc.
             'p+2',
             'p+3',
             'p+4',
             'p+5',
             'p+6',
             'p+7',
-            'result',
-        ];
-        $this->printRow($headers);
+            'result', // final, actual number of registrations
+        ]);
 
+        // Pre-process the meals into suitable intermediate variables
         $meals = Meal::with('registrations')->get();
 
         $registrations = [];
@@ -71,23 +72,29 @@ class TensorflowData extends Command
         foreach ($meals as $meal) {
             $this->debug("processing meal {$meal->id}");
 
+            // Determine sample bounds
             $sample_end = $meal->locked_timestamp->timestamp; // Samples stop here for this meal
             $sample_start = (clone $meal->locked_timestamp)->addDays(-14)->timestamp; // Samples start here for this meal
+
+            // The first few meals are missing their created_at timestamps, so
+            // we treat them as a special case for sampling, having only one sample each.
             if ($meal->id <= 44) {
                 $sample_start = $sample_end;
             }
 
+            $meal_sample_times[$meal->id] = [$sample_start, $sample_end];
+            $min_sample_start = min($min_sample_start, $sample_start);
+            $max_sample_end = max($max_sample_end, $sample_end);
 
-            $meal_sample_times[$meal->id] = array($sample_start, $sample_end);
-
-            // calc iso date of last year
+            // Calculate the same date as this meal last year, as per ISO
             $year = (int) strftime('%G', $meal->meal_timestamp->timestamp);
             $week = (int) strftime('%V', $meal->meal_timestamp->timestamp);
             $day = (int) strftime('%u', $meal->meal_timestamp->timestamp);
             $lastYearPerISO = (new Carbon)->setISODate($year - 1, $week, $day);
+
+            // Determine the initial result fields of this meal
             $meal_dayofweek[$meal->id] = $day;
             $meal_dates[$meal->id] = $meal->meal_timestamp->toISO8601String();
-
             $meal_results[$meal->id] = $meal->registrations->count();
 
             $days[$meal->id] = [
@@ -99,30 +106,22 @@ class TensorflowData extends Command
                 $this->mealFor($meals, (clone $meal->meal_timestamp)->addDays(-6))->id,    // 'c-6',
                 $this->mealFor($meals, (clone $meal->meal_timestamp)->addDays(-7))->id,    // 'c-7',
 
-                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(-7))->id,    // 'p-7',
-                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(-6))->id,    // 'p-6',
-                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(-5))->id,    // 'p-5',
-                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(-4))->id,    // 'p-4',
-                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(-3))->id,    // 'p-3',
-                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(-2))->id,    // 'p-2',
-                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(-1))->id,    // 'p-1',
-                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(+0))->id,    // 'p+0',
-                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(+1))->id,    // 'p+1',
-                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(+2))->id,    // 'p+2',
-                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(+3))->id,    // 'p+3',
-                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(+4))->id,    // 'p+4',
-                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(+5))->id,    // 'p+5',
-                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(+6))->id,    // 'p+6',
-                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(+7))->id,    // 'p+7',
+                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(-7))->id,          // 'p-7',
+                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(-6))->id,          // 'p-6',
+                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(-5))->id,          // 'p-5',
+                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(-4))->id,          // 'p-4',
+                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(-3))->id,          // 'p-3',
+                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(-2))->id,          // 'p-2',
+                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(-1))->id,          // 'p-1',
+                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(+0))->id,          // 'p+0',
+                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(+1))->id,          // 'p+1',
+                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(+2))->id,          // 'p+2',
+                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(+3))->id,          // 'p+3',
+                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(+4))->id,          // 'p+4',
+                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(+5))->id,          // 'p+5',
+                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(+6))->id,          // 'p+6',
+                $this->mealFor($meals, (clone $lastYearPerISO)->addDays(+7))->id,          // 'p+7',
             ];
-
-
-            if ($min_sample_start > $sample_start) {
-                $min_sample_start = $sample_start;
-            }
-            if ($max_sample_end < $sample_end) {
-                $max_sample_end = $sample_end;
-            }
 
             foreach ($meal->registrations as $r) {
                 $registrations[] = [$r->created_at->timestamp, $meal->id];
@@ -133,12 +132,12 @@ class TensorflowData extends Command
         uasort($registrations, function($a, $b) { return $a[0] - $b[0]; }); // Sort ascending
         $registrations = array_values($registrations);
 
-        $sample_step = 15 * 60;
-
         $this->debug("start processing samples, end = $max_sample_end");
 
+        // Sample the number of registrations per 15 minutes
+        $sampling_step = 15*60;
         $iterator = new RegistrationsIterator($registrations);
-        for($t = $min_sample_start; $t <= $max_sample_end; $t += $sample_step)
+        for($t = $min_sample_start; $t <= $max_sample_end; $t += $sampling_step)
         {
             $iterator->forwardTo($t);
 
