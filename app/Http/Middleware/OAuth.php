ee017b178603079910158301db78146091ee4fdf
Jakob Buis <jakob@jakobbuis.nl>
2018-03-28 20:38:15 +0200

    bugfix redirecting to authorization server for expired sessions

--- app/Http/Middleware/OAuth.php
+++ app/Http/Middleware/OAuth.php
@@ -38,7 +38,7 @@ class OAuth
         if ($this->oauth->valid()) {
             return $next($request);
         } else {
-            return $this->oauth->toAuthorisationServer($request->fullUrlWithQuery());
+            return $this->oauth->toAuthorisationServer($request->fullUrl());
         }
     }
 }
