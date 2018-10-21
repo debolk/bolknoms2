bac8c7e9dc25940dda59677baa5460bf6b5f90d5
Jakob Buis <jakob.buis@in10.nl>
2018-10-21 18:59:29 +0200

    move assets to the new flattened resources/ directory structure

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
 let mix = require('laravel-mix');
 
-mix.js('resources/assets/js/bootstrap.js', 'public/js/app.js').sourceMaps().version();
-mix.sass('resources/assets/sass/bootstrap.scss', 'public/css/app.css').sourceMaps().version();
+mix.js('resources/js/bootstrap.js', 'public/js/app.js').sourceMaps().version();
+mix.sass('resources/sass/bootstrap.scss', 'public/css/app.css').sourceMaps().version();
