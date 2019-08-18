261ace211d81b3e22b2c9d1103a7d5a04dd1ca7e
Jakob Buis <jakob@jakobbuis.nl>
2019-08-18 17:50:33 +0200

    default to production configuration

--- config/oauth.php
+++ config/oauth.php
@@ -2,7 +2,7 @@
 
 return [
     'endpoint' => env('OAUTH_ENDPOINT', 'https://auth.debolk.nl/'),
-    'callback' => env('OAUTH_CALLBACK', 'http://bolknoms.test/oauth/'),
+    'callback' => env('OAUTH_CALLBACK', 'http://noms.debolk.nl/oauth/'),
     'client' => [
         'id' => env('OAUTH_CLIENT_ID'),
         'secret' => env('OAUTH_CLIENT_SECRET'),
