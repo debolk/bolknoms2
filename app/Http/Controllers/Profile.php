997132162f75e0838447c740f50d622c5d874dea
Jakob Buis <jakob@jakobbuis.nl>
2019-08-20 19:23:02 +0200

    replace wrong facade-import with actual Request

--- app/Http/Controllers/Profile.php
+++ app/Http/Controllers/Profile.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\JsonResponse;
-use \Request;
+use Illuminate\Http\Request;
 
 class Profile extends Application
 {
