ba4944d462f1f68a3c47be36ff181d7d0d15e451
Jakob Buis <jakob@jakobbuis.nl>
2020-07-12 18:56:32 +0200

    make webpack less chatty about succesful builds

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,3 +2,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/bootstrap.js', 'public/js/app.js').sourceMaps().version();
 mix.sass('resources/sass/bootstrap.scss', 'public/css/app.css').sourceMaps().version();
+
+mix.disableSuccessNotifications();
