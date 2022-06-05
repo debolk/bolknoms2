7cec5d73ad88c67b226683e5ba9939804d7aba26
Shift <shift@laravelshift.com>
2022-06-05 18:13:42 +0000

    Leverage injected request object
    Laravel automatically injects the current Http [request object][1] to all Controller actions and Middleware. Leveraging this object improves consistency and testability.
    
    [1]: https://laravel.com/docs/requests#accessing-the-request

--- app/Http/Controllers/OAuth.php
+++ app/Http/Controllers/OAuth.php
@@ -2,20 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use App\Http\Helpers\OAuth as OAuthHelper;
 use App\Http\Helpers\ProfilePicture;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Request;
 
 class OAuth extends Controller
 {
     /**
      * Store the callback
      */
-    public function callback(ProfilePicture $profilePicture): RedirectResponse
+    public function callback(Request $request, ProfilePicture $profilePicture): RedirectResponse
     {
-        $result = $this->oauth->processCallback(Request::all());
+        $result = $this->oauth->processCallback($request->all());
 
         // Update the profile picture for this user
         $user = $this->oauth->user();
