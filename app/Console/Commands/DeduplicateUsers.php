9f96765b396ce3579cfd06eacefb2abc80e54dad
Jakob Buis <jakob@jakobbuis.nl>
2022-10-15 18:46:01 +0200

    created an utility command to deduplicate user entries by username, migrating registrations

---
+++ app/Console/Commands/DeduplicateUsers.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class DeduplicateUsers extends Command
+{
+    protected $signature = 'users:deduplicate {username}';
+    protected $description = 'Deduplicate a user by their username, merging registrations';
+
+    public function handle()
+    {
+        $duplicates = User::where('username', $this->argument('username'))->orderBy('created_at', 'asc')->get();
+        if ($duplicates->count() < 2) {
+            $this->info('Found no duplicates');
+            return 1;
+        }
+
+        $original = $duplicates->shift();
+
+        foreach ($duplicates as $dup) {
+            $registrations = $dup->registrations;
+            foreach ($registrations as $reg) {
+                $reg->update(['user_id' => $original->id]);
+            }
+            $this->info("Reassigned {$registrations->count()} registrations to user id {$original->id}");
+            $dup->delete();
+            $this->info("Deleted duplicate user {$user->id}");
+        }
+    }
+}
