22642c758fed5e4dd9641f7f53ab0341505de10f
Jakob Buis <jakob@jakobbuis.nl>
2025-03-12 22:03:55 +0100

    turn off CSRF

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(LoggedInUserData::class);
+        $middleware->validateCsrfTokens(except: ['*']);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
