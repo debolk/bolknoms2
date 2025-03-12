0aa275c3c50517f2a0d2c1a7ccc7ddc223ea67b9
Shift <shift@laravelshift.com>
2025-03-12 20:16:00 +0000

    Remove `webpack.mix.js`

--- webpack.mix.js
+++
@@ -1,6 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.js('resources/js/bootstrap.js', 'public/js/app.js').sourceMaps().version();
-mix.sass('resources/sass/bootstrap.scss', 'public/css/app.css').sourceMaps().version();
-
-mix.disableSuccessNotifications();
